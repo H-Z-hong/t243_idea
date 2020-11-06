8a102a7bb52d0dfbf92e61eba175c6ddb6679ae7
H-Z-hong <1815134401@qq.com>
2020-11-06 15:46:19 +0800

    本地仓库接收

---
+++ ssm/src/com/hzh/git/Demo1.java
@@ -0,0 +1,11 @@
+package com.hzh.git;
+
+/**
+ * @author hzh
+ * @create  2020-11-06 15:31
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("idea and git");
+    }
+}
